3665dcc89527f677837929a7f380c969f2dde204
ntpantoja <154106208+ntpantoja@users.noreply.github.com>
2023-12-28 16:35:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function login() {
+    // Lógica de login aqui
+    alert('Login realizado com sucesso!');
+}
+
+function cadastrar() {
+    // Lógica de cadastro aqui
+    alert('Cadastro realizado com sucesso!');
+}
